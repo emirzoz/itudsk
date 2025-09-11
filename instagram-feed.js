96e6b0fe860523d4aec20ac3dd05da62772df07b
emirzoz <54676800+emirzoz@users.noreply.github.com>
2025-09-11 23:41:02 +0300

    Update instagram-feed.js

--- instagram-feed.js
+++ instagram-feed.js
@@ -1,1 +1,34 @@
+// instagram-feed.js
+const instagramPosts = [
+  {
+    permalink: 'https://www.instagram.com/p/EXAMPLE1',
+    imageSrc: './images/post1.jpg',
+    altText: 'Instagram Post 1',
+    caption: 'BeeDigital 2025\'in hazırlıkları tüm hızıyla devam ediyor! 🎉',
+    likeCount: 158
+  },
+  {
+    permalink: 'https://www.instagram.com/p/EXAMPLE2',
+    imageSrc: './images/post2.jpg',
+    altText: 'Instagram Post 2',
+    caption: 'Yeni eğitim dönemi için harika planlarımız var! 🚀 Kulübümüze katılın ve dijital pazarlamayı birlikte keşfedin.',
+    likeCount: 210
+  },
+  {
+    permalink: 'https://www.instagram.com/p/EXAMPLE3',
+    imageSrc: './images/post3.jpg',
+    altText: 'Instagram Post 3',
+    caption: 'Sponsorluk paketlerimizle markanızı binlerce öğrenciye ulaştırın! Detaylar web sitemizde.',
+    likeCount: 95
+  },
+  // Add new posts here (newest at the bottom)
+  {
+    permalink: 'https://www.instagram.com/p/EXAMPLE4',
+    imageSrc: './images/post4.jpg',
+    altText: 'Instagram Post 4',
+    caption: 'En yeni etkinliğimizden kareler! 📸',
+    likeCount: 300
+  }
+];
 
+export default instagramPosts;
